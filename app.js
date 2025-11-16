db394d40dc49b20608daba8bc432a02d21cd9ac6
netsquarecafe12345-lgtm <netsquarecafe12345@gmail.com>
2025-11-16 12:43:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,83 +1,30 @@
-// Firebase SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+  getFirestore, 
+  collection, 
+  getDocs 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+const db = getFirestore();
 
-// Your Firebase Config
-const firebaseConfig = {
-  apiKey: "AIzaSyAN0MfvvHmznveLEoIV8vLNorRO6F2VAs0",
-  authDomain: "cashdeals-185df.firebaseapp.com",
-  projectId: "cashdeals-185df",
-  storageBucket: "cashdeals-185df.firebasestorage.app",
-  messagingSenderId: "749879075742",
-  appId: "1:749879075742:web:0fe6487fa9faedbc9aac8f"
-};
+// LOAD CLIENT DATA INTO ADMIN TABLE
+async function loadClients() {
+  const table = document.getElementById("clientTable");
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+  const querySnapshot = await getDocs(collection(db, "clients"));
+  querySnapshot.forEach((doc) => {
 
+    const data = doc.data();
 
-// -------------------------
-// SIGNUP FUNCTION
-// -------------------------
-window.signup = function() {
-  const email = document.getElementById("signupEmail").value;
-  const password = document.getElementById("signupPassword").value;
+    const row = `
+      <tr>
+        <td>${data.email || ""}</td>
+        <td>${data.username || ""}</td>
+        <td>${data.password || ""}</td>
+        <td>${data.phone || ""}</td>
+        <td>${data.balance || 0}</td>
+      </tr>
+    `;
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      alert("Admin account created!");
-      window.location.href = "login.html";
-    })
-    .catch(err => alert(err.message));
-};
-
-
-// -------------------------
-// LOGIN FUNCTION
-// -------------------------
-window.login = function() {
-  const email = document.getElementById("loginEmail").value;
-  const password = document.getElementById("loginPassword").value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "admin.html";
-    })
-    .catch(err => alert(err.message));
-};
-
-
-// -------------------------
-// LOGOUT
-// -------------------------
-window.logout = function() {
-  signOut(auth).then(() => {
-    window.location.href = "login.html";
+    table.innerHTML += row;
   });
-};
-
-
-// -------------------------
-// PROTECT ADMIN PAGE
-// -------------------------
-onAuthStateChanged(auth, user => {
-  const protectedPages = ["admin.html"];
-
-  const currentPage = window.location.pathname.split("/").pop();
-
-  if (protectedPages.includes(currentPage) && !user) {
-    window.location.href = "login.html";
-  }
-
-  if (user && currentPage === "admin.html") {
-    document.getElementById("admin-email").innerText = "Logged in as: " + user.email;
-  }
-});
+}
